7c9626a7440773cfddbcb84b163ede63639036d7
Sarkhan <david.jonquieres@gmail.com>
2022-05-31 18:08:19 +0200

    part 1

---
+++ src/components/Card.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Card = ({ movie }) => {
+  const dateFormater = (date) => {
+    let [yy, mm, dd] = date.split("-");
+    return [dd, mm, yy].join("/");
+  };
+  return (
+    <div className="card">
+      <img
+        src={
+          movie.poster_path
+            ? "https://image.tmdb.org/t/p/w500" + movie.poster_path
+            : "./img/poster.jpg"
+        }
+        alt="affiche du film"
+      />
+      <h2>{movie.title}</h2>
+      {movie.release_date ? (
+        <h5>Sorti le : {dateFormater(movie.release_date)}</h5>
+      ) : (
+        ""
+      )}
+      <h4>
+        {movie.vote_average}/10 <span>⭐</span>
+      </h4>
+    </div>
+  );
+};
+
+export default Card;
